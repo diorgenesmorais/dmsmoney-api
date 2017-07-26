1f69b9d905bee8c7ae720a9bc508ef40a80150d2
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-07-26 07:33:21 -0300

    Post lançamento

--- src/main/java/com/dms/dmsmoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/dms/dmsmoneyapi/resource/LancamentoResource.java
@@ -2,13 +2,21 @@ package com.dms.dmsmoneyapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dms.dmsmoneyapi.event.ResourceCreatedEvent;
 import com.dms.dmsmoneyapi.model.Lancamento;
 import com.dms.dmsmoneyapi.repository.LancamentoRepository;
 
@@ -19,6 +27,9 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
 	public List<Lancamento> listar() {
 		return lancamentoRepository.findAll();
@@ -28,5 +39,14 @@ public class LancamentoResource {
 	public ResponseEntity<Lancamento> buscarPorId(@PathVariable Long id) {
 		Lancamento lancamento = lancamentoRepository.findOne(id);
 		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Lancamento> nova(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, lancamentoSalvo.getId()));
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamento);
 	}
 }
