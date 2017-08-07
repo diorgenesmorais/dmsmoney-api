bf441e7e4adb9777aed0c362c1e18711d4be480e
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-08-06 23:18:34 -0300

    mais permissões de acesso

--- src/main/java/com/dms/dmsmoneyapi/resource/PessoaResource.java
+++ src/main/java/com/dms/dmsmoneyapi/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,11 +39,13 @@ public class PessoaResource {
 	private PessoaService pessoaService;
 
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
 	}
 
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Pessoa> nova(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
@@ -52,6 +55,7 @@ public class PessoaResource {
 	}
 
 	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
 	public ResponseEntity<Pessoa> buscarPorId(@PathVariable Long id) {
 		Pessoa pessoa = pessoaRepository.findOne(id);
 		return (pessoa != null) ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
@@ -59,11 +63,13 @@ public class PessoaResource {
 
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA') and #oauth2.hasScope('write')")
 	public void remover(@PathVariable Long id) {
 		pessoaRepository.delete(id);
 	}
 
 	@PutMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_ATUALIZAR_PESSOA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa) {
 		Pessoa pessoaSalva = pessoaService.atualizar(id, pessoa);
 		return ResponseEntity.ok(pessoaSalva);
@@ -71,6 +77,7 @@ public class PessoaResource {
 
 	@PutMapping("/{id}/ativo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_ATUALIZAR_PESSOA') and #oauth2.hasScope('write')")
 	public void atualizarAtivo(@PathVariable Long id, @RequestBody Boolean ativo) {
 		pessoaRepository.atualizarAtivo(id, ativo);
 	}
---
+++ src/main/resources/db/migration/V05__inserir_permissoes.sql
@@ -0,0 +1,5 @@
+-- novas regras para o sistema
+INSERT INTO permissao (id, descricao) VALUES (9, 'ROLE_REMOVER_CATEGORIA');
+INSERT INTO permissao (id, descricao) VALUES (10, 'ROLE_ATUALIZAR_CATEGORIA');
+INSERT INTO permissao (id, descricao) VALUES (11, 'ROLE_ATUALIZAR_PESSOA');
+INSERT INTO permissao (id, descricao) VALUES (12, 'ROLE_ATUALIZAR_LANCAMENTO');
